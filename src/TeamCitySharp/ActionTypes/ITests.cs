07e7c931de3c3083bc53b112c477d331882034d6
Redouane Oubenal <oubenal@gmail.com>
2019-08-17 00:22:33 +0200

    [TeamCitySharp] -  New feature
    DESCRIPTION: Add new feature to get test occurrences
    By buildLocator

---
+++ src/TeamCitySharp/ActionTypes/ITests.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TeamCitySharp.DomainEntities;
+using TeamCitySharp.Locators;
+
+namespace TeamCitySharp.ActionTypes
+{
+    public interface ITests
+    {
+        TestOccurrences ByBuildLocator(BuildLocator locator);
+    }
+}
---
+++ src/TeamCitySharp/ActionTypes/Tests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TeamCitySharp.Connection;
+using TeamCitySharp.DomainEntities;
+using TeamCitySharp.Locators;
+
+namespace TeamCitySharp.ActionTypes
+{
+    public class Tests : ITests
+    {
+        #region Attributes
+
+        private ITeamCityCaller m_caller;
+
+        #endregion
+
+        #region Constructor
+
+        internal Tests(ITeamCityCaller caller)
+        {
+            m_caller = caller;
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        public TestOccurrences ByBuildLocator(BuildLocator locator)
+        {
+            return m_caller.Get<TestOccurrences>($"/testOccurrences?locator=build:({locator})");
+        }
+
+        #endregion
+    }
+}
