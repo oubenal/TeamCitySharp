6eff55f33e52dad887b77c3acc95d156542ad3dc
Redouane Oubenal <oubenal@gmail.com>
2019-08-18 03:51:42 +0200

    indent correctly

--- src/TeamCitySharp/ActionTypes/Tests.cs
+++ src/TeamCitySharp/ActionTypes/Tests.cs
@@ -7,40 +7,40 @@ using TeamCitySharp.Locators;
 
 namespace TeamCitySharp.ActionTypes
 {
-    public class Tests : ITests
+  public class Tests : ITests
+  {
+    #region Attributes
+
+    private ITeamCityCaller m_caller;
+
+    #endregion
+
+    #region Constructor
+
+    internal Tests(ITeamCityCaller caller)
     {
-        #region Attributes
-
-        private ITeamCityCaller m_caller;
-
-        #endregion
-
-        #region Constructor
-
-        internal Tests(ITeamCityCaller caller)
-        {
-            m_caller = caller;
-        }
-
-        #endregion
-
-        #region Public Methods
-
-        public TestOccurrences ByBuildLocator(BuildLocator locator)
-        {
-            return m_caller.Get<TestOccurrences>($"/testOccurrences?locator=build:({locator})");
-        }
-
-        public TestOccurrences ByProjectLocator(ProjectLocator locator)
-        {
-            return m_caller.Get<TestOccurrences>($"/testOccurrences?locator=currentlyFailing:true,affectedProject:({locator})");
-        }
-
-        public TestOccurrences ByTestId(string testId)
-        {
-            return m_caller.Get<TestOccurrences>($"/testOccurrences?locator=test:(id:{testId})");
-        }
-
-        #endregion
+      m_caller = caller;
     }
+
+    #endregion
+
+    #region Public Methods
+
+    public TestOccurrences ByBuildLocator(BuildLocator locator)
+    {
+      return m_caller.Get<TestOccurrences>($"/testOccurrences?locator=build:({locator})");
+    }
+
+    public TestOccurrences ByProjectLocator(ProjectLocator locator)
+    {
+      return m_caller.Get<TestOccurrences>($"/testOccurrences?locator=currentlyFailing:true,affectedProject:({locator})");
+    }
+
+    public TestOccurrences ByTestId(string testId)
+    {
+      return m_caller.Get<TestOccurrences>($"/testOccurrences?locator=test:(id:{testId})");
+    }
+
+    #endregion
+  }
 }
